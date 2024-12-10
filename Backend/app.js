98c68d6c8c9e056deafbbc63ae77c009435d4ccd
hgcoder7 <119857118+hgcoder7@users.noreply.github.com>
2024-12-10 20:42:33 +0530

    user authentication

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,14 @@ const cors=require('cors');
 const express=require('express')
 const app=express();
 const userRoutes=require('./routes/user.routes')
+const cookieParser=require('cookie-parser');
 const connectToDb=require('./db/db');
 connectToDb();
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 app.get('/',(req,res)=>{
     res.send('hello')
