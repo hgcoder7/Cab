b65777e68f0db4e2efe4ad347db019a43127713a
hgcoder7 <119857118+hgcoder7@users.noreply.github.com>
2024-12-10 16:36:17 +0530

    user login

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,18 @@ dotenv.config();
 const cors=require('cors');
 const express=require('express')
 const app=express();
+const userRoutes=require('./routes/user.routes')
+const connectToDb=require('./db/db');
+connectToDb();
 
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     res.send('hello')
 })
+
+app.use('/users',userRoutes)
 
 module.exports=app;
