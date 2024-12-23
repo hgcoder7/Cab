599bb4343d3f2b231f908b004bf4a1629db9ee47
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-23 18:04:00 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,11 @@ const rideRoutes=require('./routes/ride.routes')
 const connectToDb=require('./db/db');
 connectToDb();
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://cabify-zdbf.onrender.com', // Your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Include cookies or authentication tokens
+}))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
