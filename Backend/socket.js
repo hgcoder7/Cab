3421976c0f4ba1963c396c419d2a46e74e2e8f58
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-24 01:25:06 +0530

    Update socket.js

--- Backend/socket.js
+++ Backend/socket.js
@@ -7,10 +7,8 @@ let io;
 function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
-    origin: 'https://cabify-zdbf.onrender.com', // Allow your frontend's origin
-    methods: ['GET', 'POST'], // Allowed methods
-    header: ['Content-Type', 'Authorization'], // Allow specific headers
-    credentials: true, // Allow cookies/credentials
+   origin: true,
+    credentials: true,
 }
     });
 
