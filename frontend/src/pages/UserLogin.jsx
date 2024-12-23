ce291f860c1fb017e136c593aa0c34ce6b976047
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-23 23:04:13 +0530

    Update UserLogin.jsx

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -16,12 +16,7 @@ const UserLogin = () => {
             email:email,
             password:password
         }
-        const response=await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`,userData,  {
-        headers: {
-            'Content-Type': 'application/json', // Add any custom headers here
-        },
-        withCredentials: true, // If cookies or credentials are used
-    });
+        const response=await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`,userData);
         if(response.status===200){
             const data=response.data;
             setUser(data.user);
