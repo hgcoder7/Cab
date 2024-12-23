6953625b50686b4e2f3e8091e9c031dc13547948
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-23 14:18:41 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-
+res.header( "Access-Control-Allow-Origin" );
 app.get('/',(req,res)=>{
     res.send('hello')
 })
