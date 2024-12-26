0ced436f08704d02a0e0dd1b0239345045415f6c
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-26 13:26:15 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -13,8 +13,10 @@ const connectToDb=require('./db/db');
 connectToDb();
 
 app.use(cors({
-    origin: '*',
-    methods: ["GET", "POST"],
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
