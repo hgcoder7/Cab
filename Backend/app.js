d42902adf86f515be45bc509ea7dc2fb4830b775
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-26 13:35:06 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,12 +12,7 @@ const rideRoutes=require('./routes/ride.routes')
 const connectToDb=require('./db/db');
 connectToDb();
 
-app.use(cors({
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
