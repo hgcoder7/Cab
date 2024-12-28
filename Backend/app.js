6edce6b4ed1161c9a07ad9677ef05fc80cb26133
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-29 01:21:32 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -17,6 +17,13 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://cabify-zdbf.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
