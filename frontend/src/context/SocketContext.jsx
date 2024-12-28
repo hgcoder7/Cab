9626cdcbcdfe63082defe07bece712e257ca72f3
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-29 02:28:01 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,7 +4,10 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL}`); // Replace with your server URL
+const socket = io(`${import.meta.env.VITE_BASE_URL}`, {
+    withCredentials: true,
+    transports: ['websocket', 'polling']
+}); // Replace with your server URL
 
 const SocketProvider = ({ children }) => {
     useEffect(() => {
