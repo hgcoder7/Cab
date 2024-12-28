8d1ddac459e6ce640464742fdb3088dbf843cc5e
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-12-29 02:03:03 +0530

    Update user.routes.js

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -4,12 +4,12 @@ const {body}=require('express-validator')
 const userController= require('../controllers/user.controller')
 const authMiddleware=require('../middlewares/auth.middleware')
 
-// router.post('/register',[
-//     body('email').isEmail().withMessage('Invalid Email'),
-//     body('fullname.firstname').isLength({min:3}).withMessage('first name must be at least 3 character'),
-//     body('password').isLength({min:6}).withMessage('Password must be at least 6 character')
-// ],
-// userController.registerUser)
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('first name must be at least 3 character'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 character')
+],
+userController.registerUser)
 router.post('/login', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
@@ -21,12 +21,12 @@ router.post('/login', [
 }, userController.loginUser);
 
 
-router.post('/login',[
-    body('email').isEmail().withMessage('Invalid Email'),
-    body('password').isLength({min:6}).withMessage('Password must be at least 6 character')
-],
-userController.loginUser
-);
+// router.post('/login',[
+//     body('email').isEmail().withMessage('Invalid Email'),
+//     body('password').isLength({min:6}).withMessage('Password must be at least 6 character')
+// ],
+// userController.loginUser
+// );
 
 router.get('/profile',authMiddleware.authUser, userController.getUserProfile);
 router.get('/logout',authMiddleware.authUser, userController.logoutUser);
